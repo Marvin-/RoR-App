1520f7cc3f2f60c69be464e1b461ec0d4b7cf38e
Marvin M <marvin23m@gmail.com>
2016-03-04 20:12:31 +0000

    Migrate User Model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup 
+    @user = User.new
+  end
+
+  test "should be valid" do 
+    assert @user.valid?
+  end
 end
