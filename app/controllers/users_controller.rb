de4cb273e3107a099dd46dda02ddc2b7d995dd1c
Marvin M <marvin23m@gmail.com>
2016-03-06 18:32:59 +0000

    Build create action. User private method for params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
   end
 
   def create 
+    @user = User.new(user_params)
+    @user.save
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
