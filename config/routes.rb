b85a9410b15a582c52e0a9b072d36c2fa0b07e36
Marvin M <marvin23m@gmail.com>
2016-03-08 09:13:53 +0000

    Use full routing resource for User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :users, :only => [:new, :create]
+  resources :users
+  # , :only => [:new, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
