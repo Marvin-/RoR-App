6070808683c4c9b3881773c855220d89757351e5
Marvin M <marvin23m@gmail.com>
2016-03-08 09:17:49 +0000

    Update controller to handle existing User and update User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
   def create 
     # @user = User.new(username: params[:username], :email => params[:email],
                      # :password => params[:password])
@@ -14,6 +19,18 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    
+    redirect_to user_path
   end
 
   def user_params
