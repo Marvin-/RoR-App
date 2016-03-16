aa7eb1c8b08dd5fb71146e6f15ff1bb30426a7d3
Marvin M <marvin23m@gmail.com>
2016-03-16 17:12:54 +0000

    Write integration test and watch it pass

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid signup info" do
+    get signup_path
+    assert_no_difference 'User.count' do 
+      post users_path, user: { username: '', 
+                               email: 'user@invalid',
+                               password: 'foobar' }
+    end
+    assert_template 'users/new'
+  end 
 end
