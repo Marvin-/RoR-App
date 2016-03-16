207b91f8ae86c47d5df2e1126c44639a5604172e
Marvin M <marvin23m@gmail.com>
2016-03-16 17:12:23 +0000

    Add shortcut route signup for User#new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
-
+  get 'signup' => 'users#new'
   resources :users
   # , :only => [:new, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
