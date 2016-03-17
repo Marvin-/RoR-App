94c1de246a5adcb09dec03938d41b218df6cbbdf
Marvin M <marvin23m@gmail.com>
2016-03-17 00:54:04 +0000

    Test valid signups

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,4 +10,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end 
+  test 'valid signup info' do 
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { username: 'Test Name', 
+                                            email: 'test@yahoo.com',
+                                            password: 'foobar' } 
+    end
+    assert_template 'users/show'
+  end
 end
