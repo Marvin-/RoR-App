b650fe5e769aa7d876c1ff95295642ff7d898a5b
Marvin M <marvin23m@gmail.com>
2016-03-18 16:50:59 +0000

    Add routes for Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'signup' => 'users#new'
+  root                  'static_pages#home'
+  get     'help' =>     'static_pages#help'
+  get     'signup' =>   'users#new'
+  get     'login' =>    'sessions#new'
+  post    'login' =>    'sessions#create'
+  delete  'logout' =>   'sessions#destroy'
   resources :users
   # , :only => [:new, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
