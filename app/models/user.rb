c337e54dc96485a116db063170ecf553b08f6213
Marvin M <marvin23m@gmail.com>
2016-03-20 11:21:05 +0000

    Add digest method for use in fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }
 
-  # has_secure_password
+  has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
