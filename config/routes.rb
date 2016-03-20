cf5cfb01af0bdb163ebdc0209141273a7bd147df
Marvin M <marvin23m@gmail.com>
2016-03-20 11:14:10 +0000

    Add contact route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root                  'static_pages#home'
-  get     'help' =>     'static_pages#help'
+  get     'contact' =>     'static_pages#contact'
   get     'signup' =>   'users#new'
   get     'login' =>    'sessions#new'
   post    'login' =>    'sessions#create'
