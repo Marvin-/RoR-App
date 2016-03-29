00accdceed37fb1e604b493d74ce6ceca24656f0
Marvin M <marvin23m@gmail.com>
2016-03-29 11:03:40 +0100

    Correct the 'remember user' method. Add 'forget' method to log out from persistent session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,8 @@ module SessionsHelper
   
   def remember(user)
     user.remember
-    cookies.parmanent.signed[:user_id] = user.id
-    cookies.parmanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
@@ -26,7 +26,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:uder_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out 
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
