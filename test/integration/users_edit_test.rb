2af1ac9f3eea9b8ae4a6b504cadffc3dc6d73c22
Marvin M <marvin23m@gmail.com>
2016-03-29 14:20:27 +0100

    Add test for successful edit.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -15,4 +15,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
+  test "succesful edit" do 
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { username: name, 
+                                    email: email, 
+                                    password: "", 
+                                    password_confirmation: ""}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload 
+    assert_equal name, @user.username
+    assert_equal email, @user.email
+  end
 end
