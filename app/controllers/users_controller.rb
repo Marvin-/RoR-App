c52e3e58b14c0b87c8278b6c2604ca4de175882b
Marvin M <marvin23m@gmail.com>
2016-03-29 14:40:46 +0100

    Update user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to user_path
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
