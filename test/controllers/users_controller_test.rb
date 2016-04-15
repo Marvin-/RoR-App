1d4570c41173a7a14307772434e08d295d8a2d2b
Marvin M <marvin23m@gmail.com>
2016-04-15 16:01:13 +0100

    Test edit and update actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:marvin)
+  end
+
+  test "should redirect edit when not logged in" do 
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
+
+  test "should redirect update when not logged in" do 
+    patch :update, id: @user, user: { name: @user.username, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
 end
