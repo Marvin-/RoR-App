ea0b8016af427f1a0df6d72b2de8dfe24462eb7b
Marvin M <marvin23m@gmail.com>
2016-04-21 14:11:23 +0100

    Test for trying to edit wrong user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:marvin)
+    @user2 = users(:queen)
   end
 
   test "should redirect edit when not logged in" do 
@@ -15,5 +16,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch :update, id: @user, user: { name: @user.username, email: @user.email }
     assert_not flash.empty?
     assert_redirected_to login_path
+  end
+
+  test "should redirect edit when logged in as wrong user" do 
+    log_in_as(@user2)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do 
+    log_in_as(@user2)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
