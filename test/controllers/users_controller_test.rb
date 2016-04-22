71a1db726e9145379c22305f8dab70a621d10d4f
Marvin M <marvin23m@gmail.com>
2016-04-22 15:45:12 +0100

    Test: Should redirect index when not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user2 = users(:queen)
   end
 
+
+  test "should redirect index when not logged in" do 
+    get :index
+    assert_redirected_to login_url
+  end
+
   test "should redirect edit when not logged in" do 
     get :edit, id: @user
     assert_not flash.empty?
